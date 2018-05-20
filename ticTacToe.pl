fc3650299be0778f67f4f2041c966c5c9e5f5c08
Michael Kurowski <Byamarro@gmail.com>
2018-05-20 02:05:20 +0200

    First rule added

--- ticTacToe.pl
+++ ticTacToe.pl
@@ -1,2 +1,11 @@
 likes(maria,tom).
 likes(tom,maria).
+likes(tom, books).
+likes(maria, books).
+father(tom, maria).
+father(tom, tiffany).
+
+sibling(X, Y) :- 
+	father(Z, X), father(Z, Y).
+
+mact(A, B) :- likes(A, B).
